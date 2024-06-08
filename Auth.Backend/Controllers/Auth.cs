0b51457e640dfe124e2b2c54237dbb24231709d7
Victor Luiz <victorluizcefet@gmail.com>
2024-06-08 13:36:47 -0300

    Add auth test

---
+++ Auth.Backend/Controllers/Auth.cs
@@ -0,0 +1,40 @@
+﻿using Auth.Backend.Model.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Auth.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpPost()]
+        [ProducesResponseType(typeof(LoginRequest), StatusCodes.Status200OK)]
+        public IActionResult LoginUser(LoginRequest loginRequest)
+        {
+            if(loginRequest == null || (!loginRequest.UserName.Equals("victorluizskt") && !loginRequest.Password.Equals("123456")))
+            {
+                return BadRequest("User or password invalid.");
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var Sectoken = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+            );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+            return Ok(token);
+        }
+    }
+}
--- Auth.Backend/Program.cs
+++ Auth.Backend/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
 var app = builder.Build();
 
